1e61357216b967262662d9d879cc80ee5e3cd1a8
876692 <premvarathen.ramansundaram@aa.com>
2018-11-03 22:33:03 -0500

    autowired the service

--- src/test/java/features/common/steps/AppConfiguration.java
+++ src/test/java/features/common/steps/AppConfiguration.java
@@ -1,17 +1,20 @@
 package features.common.steps;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.ui.velocity.VelocityEngineFactory;
 
 import com.cobrand.ct.CobrandAcceptanceApplication;
 import com.cobrand.ct.action.CobrandAction;
 import com.cobrand.ct.action.PassengerAction;
 import com.cobrand.ct.action.PassengerEligibilityAction;
-
+import com.cobrand.ct.service.CobrandService;
+import com.cobrand.ct.service.impl.CobrandServiceImpl;
 import com.cobrand.ct.utils.ActionUtils;
 @SpringBootTest(classes = CobrandAcceptanceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
@@ -33,6 +36,21 @@ public class AppConfiguration {
 	@Bean
     public PassengerEligibilityAction passengerEligibilityAction() {
         return new PassengerEligibilityAction();
+    }
+	
+	@Bean
+    public CobrandService cobrandService() {
+        return new CobrandServiceImpl();
+    }
+	
+	@Bean
+	public VelocityEngine getVelocityEngine() throws VelocityException, IOException{
+        VelocityEngineFactory factory = new VelocityEngineFactory();
+        Properties props = new Properties();
+        props.put("resource.loader", "class");
+        props.put("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        factory.setVelocityProperties(props);
+        return factory.createVelocityEngine();      
     }
 	
 }
